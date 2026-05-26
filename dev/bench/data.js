e8a53df0bda57c2cf5692e5de333fd18f19187a1
github-action-benchmark <github@users.noreply.github.com>
2026-05-26 20:04:05 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 1192d1b114b75e56357adc79e09d3c3fe30317fc

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1779825845956,
+  "repoUrl": "https://github.com/l1a/retch",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "634380+l1a@users.noreply.github.com",
+            "name": "Ken Tobias",
+            "username": "l1a"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1192d1b114b75e56357adc79e09d3c3fe30317fc",
+          "message": "Merge pull request #39 from l1a/fix/benchmark-ci\n\nFix continuous benchmarking CI format",
+          "timestamp": "2026-05-26T13:00:52-07:00",
+          "tree_id": "8f08ba7318e87e1b95ba9197791b51917e34bf4d",
+          "url": "https://github.com/l1a/retch/commit/1192d1b114b75e56357adc79e09d3c3fe30317fc"
+        },
+        "date": 1779825845647,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SystemInfo__collect",
+            "value": 85830035.19514295,
+            "unit": "ns"
+          },
+          {
+            "name": "gpu__detect_gpus",
+            "value": 2356509.892689231,
+            "unit": "ns"
+          }
+        ]
+      }
+    ]
+  }
+}
