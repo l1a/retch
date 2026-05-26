927e8060c1c0d6422b44248c405783e1330ab048
github-action-benchmark <github@users.noreply.github.com>
2026-05-26 20:30:56 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for e9fede0fb8621ddc70e274320a1f31222cbdbe1f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779825845956,
+  "lastUpdate": 1779827456287,
   "repoUrl": "https://github.com/l1a/retch",
   "entries": {
     "Benchmark": [
@@ -33,6 +33,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "gpu__detect_gpus",
             "value": 2356509.892689231,
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "634380+l1a@users.noreply.github.com",
+            "name": "Ken Tobias",
+            "username": "l1a"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e9fede0fb8621ddc70e274320a1f31222cbdbe1f",
+          "message": "Merge pull request #41 from l1a/fix/benchmark-fastfetch-install\n\nFix fastfetch installation in benchmark workflow",
+          "timestamp": "2026-05-26T13:24:33-07:00",
+          "tree_id": "69e3e0ec4a61b3a4bc41dd9e6d3c74372c48e095",
+          "url": "https://github.com/l1a/retch/commit/e9fede0fb8621ddc70e274320a1f31222cbdbe1f"
+        },
+        "date": 1779827455490,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CLI execution - fastfetch",
+            "value": 10908583.280000001,
+            "unit": "ns"
+          },
+          {
+            "name": "CLI execution - retch",
+            "value": 157147071.08,
+            "unit": "ns"
+          },
+          {
+            "name": "SystemInfo__collect",
+            "value": 157395909.24999994,
+            "unit": "ns"
+          },
+          {
+            "name": "gpu__detect_gpus",
+            "value": 2346709.3797579617,
             "unit": "ns"
           }
         ]
