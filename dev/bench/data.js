e50c7d1b6c8176a93918593fd5851052bb41479a
github-action-benchmark <github@users.noreply.github.com>
2026-06-04 15:45:10 +0000

    add Windows x64 Benchmarks (customSmallerIsBetter) benchmark result for 046c8e0b0137a548dcb51d46fef7703b46efe9cc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780587451252,
+  "lastUpdate": 1780587910296,
   "repoUrl": "https://github.com/l1a/retch",
   "entries": {
     "Linux x64 Benchmarks": [
@@ -1371,6 +1371,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "systeminfo__collect",
             "value": 1650082230,
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "634380+l1a@users.noreply.github.com",
+            "name": "Ken Tobias",
+            "username": "l1a"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "046c8e0b0137a548dcb51d46fef7703b46efe9cc",
+          "message": "v0.3.3 - Native OS Queries & Performance Optimization (#68)\n\n* Implement native queries for route, displays, reg\n\n* Allow clippy upper_case_acronyms for win_reg\n\n* Bump version to 0.3.3 and rebuild man pages\n\n* Test route parsing and wire up helper\n\n* Restrict route parser to Linux or tests",
+          "timestamp": "2026-06-04T08:16:04-07:00",
+          "tree_id": "969803f451b805de37c1b56fbebdded3f6c125d3",
+          "url": "https://github.com/l1a/retch/commit/046c8e0b0137a548dcb51d46fef7703b46efe9cc"
+        },
+        "date": 1780587901599,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CLI execution - fastfetch",
+            "value": 33955554.00000001,
+            "unit": "ns"
+          },
+          {
+            "name": "CLI execution - retch",
+            "value": 1658560584,
+            "unit": "ns"
+          },
+          {
+            "name": "gpu__detect_gpus",
+            "value": 297262237.5,
+            "unit": "ns"
+          },
+          {
+            "name": "systeminfo__collect",
+            "value": 1579978510,
             "unit": "ns"
           }
         ]
