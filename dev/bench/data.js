7f58d1e97b1825a23b7aa0dfff4473c79f40d254
github-action-benchmark <github@users.noreply.github.com>
2026-06-04 16:12:13 +0000

    add Linux x64 Benchmarks (customSmallerIsBetter) benchmark result for 585270b923d52522281c533fb0763203d8a90b40

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780588282123,
+  "lastUpdate": 1780589533801,
   "repoUrl": "https://github.com/l1a/retch",
   "entries": {
     "Linux x64 Benchmarks": [
@@ -351,6 +351,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "gpu__detect_gpus",
             "value": 1746108.2827593226,
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "634380+l1a@users.noreply.github.com",
+            "name": "Ken Tobias",
+            "username": "l1a"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "585270b923d52522281c533fb0763203d8a90b40",
+          "message": "Update CLI flags for short, long, and mode options (#70)",
+          "timestamp": "2026-06-04T09:08:05-07:00",
+          "tree_id": "0ea02c92aac1141943ac5f8df9742e9e9034f14f",
+          "url": "https://github.com/l1a/retch/commit/585270b923d52522281c533fb0763203d8a90b40"
+        },
+        "date": 1780589533525,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CLI execution - fastfetch",
+            "value": 3185817.520000001,
+            "unit": "ns"
+          },
+          {
+            "name": "CLI execution - retch",
+            "value": 54311254.019999996,
+            "unit": "ns"
+          },
+          {
+            "name": "SystemInfo__collect",
+            "value": 51265993.71993153,
+            "unit": "ns"
+          },
+          {
+            "name": "gpu__detect_gpus",
+            "value": 1744142.2546284464,
             "unit": "ns"
           }
         ]
