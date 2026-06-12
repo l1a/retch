979493bd84e6cd12cc330b0b1507a779d070d664
github-action-benchmark <github@users.noreply.github.com>
2026-06-12 04:40:20 +0000

    add Linux Arm64 Benchmarks (customSmallerIsBetter) benchmark result for 0fa34c02ae6023fc7850129a4f7276016a617ff1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781239214898,
+  "lastUpdate": 1781239220257,
   "repoUrl": "https://github.com/l1a/retch",
   "entries": {
     "Local - Linux x64 (real hardware)": [
@@ -3818,6 +3818,85 @@ window.BENCHMARK_DATA = {
           {
             "name": "network__parse_proc_net_route",
             "value": 252.10516986890053,
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "634380+l1a@users.noreply.github.com",
+            "name": "Ken Tobias",
+            "username": "l1a"
+          },
+          "committer": {
+            "email": "634380+l1a@users.noreply.github.com",
+            "name": "Ken Tobias",
+            "username": "l1a"
+          },
+          "distinct": true,
+          "id": "0fa34c02ae6023fc7850129a4f7276016a617ff1",
+          "message": "docs: add just man step to release checklist in AGENTS.md",
+          "timestamp": "2026-06-11T21:28:57-07:00",
+          "tree_id": "66575adca6ab2319c65bcf709c017fcdc86814fa",
+          "url": "https://github.com/l1a/retch/commit/0fa34c02ae6023fc7850129a4f7276016a617ff1"
+        },
+        "date": 1781239219733,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CLI execution - fastfetch",
+            "value": 1706630.7399999998,
+            "unit": "ns"
+          },
+          {
+            "name": "CLI execution - retch",
+            "value": 79825154.74,
+            "unit": "ns"
+          },
+          {
+            "name": "SystemInfo__collect",
+            "value": 77586999.19066058,
+            "unit": "ns"
+          },
+          {
+            "name": "audio__parse_asound_cards",
+            "value": 994.1767598254661,
+            "unit": "ns"
+          },
+          {
+            "name": "display__parse_monitor_name_from_edid",
+            "value": 55.47560986623289,
+            "unit": "ns"
+          },
+          {
+            "name": "display__parse_refresh_rate_from_edid",
+            "value": 2.94667076380881,
+            "unit": "ns"
+          },
+          {
+            "name": "display__parse_serial_number_from_edid",
+            "value": 49.07638314582824,
+            "unit": "ns"
+          },
+          {
+            "name": "display__parse_xrandr_displays",
+            "value": 7711.789683517107,
+            "unit": "ns"
+          },
+          {
+            "name": "gpu__detect_gpus",
+            "value": 1188385.1286112599,
+            "unit": "ns"
+          },
+          {
+            "name": "network__parse_iw_link_output",
+            "value": 356.82343224324126,
+            "unit": "ns"
+          },
+          {
+            "name": "network__parse_proc_net_route",
+            "value": 249.06967589912742,
             "unit": "ns"
           }
         ]
