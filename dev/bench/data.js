2a1d53451314b363bde9aff6b73008bf0c9f01d6
github-action-benchmark <github@users.noreply.github.com>
2026-06-18 01:11:01 +0000

    add Windows Arm64 Benchmarks (customSmallerIsBetter) benchmark result for 05af388aff244d23650c76125cf654f8e8c0e378

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781744745568,
+  "lastUpdate": 1781745061159,
   "repoUrl": "https://github.com/l1a/retch",
   "entries": {
     "Local - Linux x64 (real hardware)": [
@@ -16559,6 +16559,80 @@ window.BENCHMARK_DATA = {
           {
             "name": "systeminfo__collect",
             "value": 1421660700,
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "634380+l1a@users.noreply.github.com",
+            "name": "Ken Tobias",
+            "username": "l1a"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "05af388aff244d23650c76125cf654f8e8c0e378",
+          "message": "Update reporting process instructions in SECURITY.md",
+          "timestamp": "2026-06-17T17:34:53-07:00",
+          "tree_id": "042e6e58c597f41829fa63d91478a53a73937965",
+          "url": "https://github.com/l1a/retch/commit/05af388aff244d23650c76125cf654f8e8c0e378"
+        },
+        "date": 1781745058440,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CLI execution - fastfetch",
+            "value": 280336300,
+            "unit": "ns"
+          },
+          {
+            "name": "CLI execution - retch",
+            "value": 1567642450,
+            "unit": "ns"
+          },
+          {
+            "name": "display__parse_monitor_name_from_edid",
+            "value": 97.90963843619448,
+            "unit": "ns"
+          },
+          {
+            "name": "display__parse_refresh_rate_from_edid",
+            "value": 2.9469113444660593,
+            "unit": "ns"
+          },
+          {
+            "name": "display__parse_serial_number_from_edid",
+            "value": 98.2976546440992,
+            "unit": "ns"
+          },
+          {
+            "name": "fetch__format_cpu_cores",
+            "value": 78.14252417068332,
+            "unit": "ns"
+          },
+          {
+            "name": "gpu__detect_gpus",
+            "value": 44967.714783982505,
+            "unit": "ns"
+          },
+          {
+            "name": "network__parse_iw_link_output",
+            "value": 476.4395515591318,
+            "unit": "ns"
+          },
+          {
+            "name": "network__parse_netsh_output",
+            "value": 736.6245061329332,
+            "unit": "ns"
+          },
+          {
+            "name": "systeminfo__collect",
+            "value": 1368899060,
             "unit": "ns"
           }
         ]
